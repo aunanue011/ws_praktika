c5907313495376a4d155b4093b3aff502430080f
asier <aunanue011@gmail.com>
2015-12-09 13:55:19 +0100

    Login eginda

---
+++ datuBasea/proba.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" type="text/css" href="../js/abixuak/dist/sweetalert2.css" />
+<script src="../js/jquery.js"></script>
+<body bgcolor="#7fffd4">
+<script src="../js/abixuak/dist/sweetalert2.min.js" ></script>
+<script language="JavaScript">
+    swal({
+            title: "Uuyy.....",
+            text: "Baimenik gabeko erabiltzailea, edo,  erabiltzaile eta pasahitz okerrak.",
+            type: "error"
+        },
+        function(){
+            window.location.href = 'index.jsp';
+        });
+
+    </script>
+</body>
